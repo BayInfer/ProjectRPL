1cb6d7c495810ceb8d33c41087ad4c97b2c33ef0
BayInfer <148402929+BayInfer@users.noreply.github.com>
2024-06-24 14:41:24 +0700

    Add files via upload

---
+++ about-us.php
@@ -0,0 +1,80 @@
+<?php
+include('includes/config.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>GAMESUHUY | About us</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/modern-business.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <?php include('includes/header.php');?>
+    <!-- Page Content -->
+    <div class="container">
+
+<?php 
+$pagetype='aboutus';
+$query=mysqli_query($con,"select PageTitle,Description from tblpages where PageName='$pagetype'");
+while($row=mysqli_fetch_array($query))
+{
+
+?>
+      <br>
+      <br>
+      <h1 class="title"><?php echo htmlentities($row['PageTitle'])?>
+  
+      </h1>
+
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <a href="index.php" class="home-item">Home</a>
+        </li>
+        <li class="breadcrumb-item active">About</li>
+      </ol>
+
+      <br>
+      <br>
+
+      <!-- Intro Content -->
+      <div class="row">
+
+        <div class="description-about">
+
+          <p><?php echo $row['Description'];?></p>
+        </div>
+      </div>
+      <!-- /.row -->
+<?php } ?>
+    
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+ <?php include('includes/footer.php');?>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+    <footer class="py-5">
+      <div class="container">
+        <p class="m-0 text-center text-white">Project RPL Abyan Makarim</p>
+      </div>
+      <!-- /.container -->
+    </footer>
